9641f3800e50629118e90eb9586c78bd0ea32197
Jesus Santiago <jesus-contreras1@eep-igroup.com>
2025-01-28 17:05:41 +0100

    ADD sho movile - complete

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 console.log('Happy developing ✨')
+
+const settingSection = document.getElementById('settingSection');
+const settingBtn = document.getElementById("settingBtn")
+    .addEventListener("click", (e) => {
+    e.preventDefault();
+    console.log('clicked1');
+    settingSection.style.display = 'flex';
+});
+
+const closeSetting = document.getElementById("closeSetting")
+    .addEventListener('click', (e) => {
+        e.preventDefault();
+        console.log('clicked');
+        settingSection.style.display = 'none';
+})
+
